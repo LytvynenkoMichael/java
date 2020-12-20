a13d929db143cb7ff081cf1350207a0d67514976
LytvynenkoMichael <lytvynenko.michael@gmail.com>
2020-12-20 13:46:29 +0200

    first classwork

---
+++ classwork/Operation.java
@@ -0,0 +1,14 @@
+package classwork;
+
+public abstract class Operation {
+
+    public abstract Number sum(Number a, Number b);
+
+    public abstract Number multiplication(Number a, Number b);
+
+    public abstract Number divide(Number a, Number b);
+
+    public abstract Number difference(Number a, Number b);
+
+    public abstract Number mod(Number a, Number b);
+}
